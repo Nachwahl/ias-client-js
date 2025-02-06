8af9733551d1094d280bb9c990fa5618f2fa6696
zafaco GmbH <info@zafaco.de>
2025-02-06 17:10:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# ias-client-js #
+
+Internet Access Speed (IAS) JavaScript measurement library. The repository is used by the service [breitbandmessung.de](https://breitbandmessung.de) of the German Federal Network Agency (Bundesnetzagentur), and originates in the BEREC [Net Neutrality (NN) Reference Measurement System](https://github.com/net-neutrality-tools/nntool).
+
+### Execution ###
+
+1. Open `src/index.html`
+
+---------------
+
+# License #
+
+`ias-client-js` is released under the [AGPLv3](https://www.gnu.org/licenses/agpl-3.0.txt)
+
+Copyright (C) 2016-2025 zafaco GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3 
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ src/config.js
@@ -0,0 +1,63 @@
+  /*
+  Copyright (C) 2016-2025 zafaco GmbH
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License version 3 
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http:   //www.gnu.org/licenses/>.
+*/
+
+var WSConfig = function ()
+{
+    return (this);
+};
+
+
+WSConfig.prototype.getMeasurementParameters = function (testCase, platform)
+{
+    var measurementParameters = {};
+
+    measurementParameters.cmd      = 'start';
+    measurementParameters.platform = platform;
+
+    measurementParameters.wsTargets = [];
+    measurementParameters.wsTargets.push('peer');
+
+    measurementParameters.wsTLD        = 'example.com';
+    measurementParameters.wsTargetPort = '443';
+    measurementParameters.wsWss        = 1;
+
+    measurementParameters.wsWorkerPath               = 'worker.js';
+    measurementParameters.performRouteToClientLookup = true;
+
+    if (testCase === 'all')
+    {
+        measurementParameters.performRttMeasurement      = true;
+        measurementParameters.performDownloadMeasurement = true;
+        measurementParameters.performUploadMeasurement   = true;
+    }
+
+    if (testCase === 'rtt')
+    {
+        measurementParameters.performRttMeasurement = true;
+    }
+
+    if (testCase === 'download')
+    {
+        measurementParameters.performDownloadMeasurement = true;
+    }
+
+    if (testCase === 'upload')
+    {
+        measurementParameters.performUploadMeasurement = true;
+    }
+
+    return measurementParameters;
+};
